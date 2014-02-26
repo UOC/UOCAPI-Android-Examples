a2179eb681199e7b766018ad057b69e4d5d26114
ManuelPerera <manuel.perera.paquico@gmail.com>
2014-02-26 12:56:57 +0100

    Rename Classrooms_IG
    Rename Classrooms_IG

---
+++ Classrooms_IG/Classrooms_IG/src/main/java/com/uoc/classrooms_ig/MainActivity.java
@@ -0,0 +1,25 @@
+package com.uoc.classrooms_ig;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.uoc.openapilibrary.LoginActivity;
+
+public class MainActivity extends LoginActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(com.uoc.classrooms_ig.R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public Intent NextActivityIntent() {
+        return new Intent (this, GetClassroomsActivity.class);
+    }
+
+
+}
