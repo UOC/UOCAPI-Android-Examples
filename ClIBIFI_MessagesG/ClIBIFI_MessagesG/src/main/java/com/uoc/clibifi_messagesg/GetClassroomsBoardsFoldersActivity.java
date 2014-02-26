98f90aca7192584f624de22d5e36524bbe544e33
ManuelPerera <manuel.perera.paquico@gmail.com>
2014-02-26 10:36:50 +0100

    Rename ClIBIFI_MessagesG
    Rename ClIBIFI_MessagesG

---
+++ ClIBIFI_MessagesG/ClIBIFI_MessagesG/src/main/java/com/uoc/clibifi_messagesg/GetClassroomsBoardsFoldersActivity.java
@@ -0,0 +1,106 @@
+package com.uoc.clibifi_messagesg;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.uoc.openapilibrary.Utils;
+import com.uoc.openapilibrary.model.Folder;
+import com.uoc.openapilibrary.model.FolderList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Manuel on 3/12/13.
+ */
+public class GetClassroomsBoardsFoldersActivity extends Activity{
+    Folder f;
+    String Cid;
+    String Bid;
+    List<Folder> listF;
+    final ArrayList<String> list = new ArrayList<String>();
+    StableArrayAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_classrooms);
+        final ListView listview = (ListView) findViewById(R.id.listView1);
+
+        adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+        Intent myIntent= getIntent();
+        Cid = myIntent.getStringExtra("CID");
+        Bid = myIntent.getStringExtra("BID");
+        String[] params = new String[3];
+        params[0] = Utils.getToken();
+        params[1] = Cid;
+        params[2] = Bid;
+        new LoadClassroomTask().execute(params);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int pos, long id) {
+                f = listF.get(pos);
+                Intent newintent = new Intent (getApplicationContext(), GetClassroomsBoardsFoldersMessagesActivity.class);
+                newintent.putExtra("FID", f.getId());
+                newintent.putExtra("CID", Cid);
+                newintent.putExtra("BID", Bid);
+                GetClassroomsBoardsFoldersActivity.this.startActivity(newintent);
+
+            }
+        });
+    }
+
+    private class LoadClassroomTask extends AsyncTask<String, Void, FolderList> {
+        protected void onPostExecute(FolderList result) {
+            if (result != null) {
+                ArrayList<Folder> aux = result.getFolders();
+                listF = aux;
+                for (int i = 0; i < aux.size(); ++i) {
+                    list.add(aux.get(i).getName());
+                }
+                adapter.notifyDataSetChanged();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Hay problemas de conexion", Toast.LENGTH_LONG).show();
+            }
+        }
+        @Override
+        protected FolderList doInBackground(String... param) {
+            return FolderList.getFolderListfromClassRoomBoardWS(param[0], param[1], param[2]);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.view_classrooms, menu);
+        return true;
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+    }
+}
---
+++ ClIBIFI_MessagesG/ClIBIFI_MessagesG/src/main/java/com/uoc/clibifi_messagesg/ViewMessageActivity.java
@@ -0,0 +1,80 @@
+package com.uoc.clibifi_messagesg;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.uoc.openapilibrary.Utils;
+import com.uoc.openapilibrary.model.Message;
+
+public class ViewMessageActivity extends Activity{
+    TextView tvId;
+    TextView tvSubject;
+    TextView tvSnippet;
+    TextView tvDate;
+    TextView tvColor;
+    TextView tvStatus;
+    TextView tvFrom;
+    TextView tvTo;
+    TextView tvCC;
+    TextView tvBody;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_message);
+        Intent myIntent= getIntent();
+
+        String[] params = new String[5];
+        params[0] = Utils.getToken();
+        params[1] = myIntent.getStringExtra("CID");
+        params[2] = myIntent.getStringExtra("BID");
+        params[3] = myIntent.getStringExtra("FID");
+        params[4] = myIntent.getStringExtra("MID");
+        new LoadEventTask().execute(params);
+    }
+
+    private class LoadEventTask extends AsyncTask<String, Void, Message> {
+        protected void onPostExecute(Message result) {
+            if (result != null) {
+                tvId = (TextView)findViewById(R.id.tvId);
+                tvId.setText(result.getId());
+                tvSubject = (TextView)findViewById(R.id.tvSubject);
+                tvSubject.setText(result.getSubject());
+                tvSnippet = (TextView)findViewById(R.id.tvSnippet);
+                tvSnippet.setText(result.getSnippet());
+                tvDate = (TextView)findViewById(R.id.tvDate);
+                tvDate.setText(result.getDate());
+                tvColor = (TextView)findViewById(R.id.tvColor);
+                tvColor.setText(Long.toString(result.getColor()));
+                tvStatus = (TextView)findViewById(R.id.tvStatus);
+                tvStatus.setText(Long.toString(result.getStatus()));
+                tvFrom = (TextView)findViewById(R.id.tvFrom);
+                tvFrom.setText(result.getFrom());
+                tvTo = (TextView)findViewById(R.id.tvTo);
+                tvTo.setText(result.getTo());
+                tvCC = (TextView)findViewById(R.id.tvCC);
+                tvCC.setText(result.getCc());
+                tvBody = (TextView)findViewById(R.id.tvBody);
+                tvBody.setText(result.getBody());
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Hay problemas de conexion", Toast.LENGTH_LONG).show();
+            }
+        }
+        @Override
+        protected Message doInBackground(String... param) {
+            return Message.getMessagefromClassRoomBoardFolderWS(param[0], param[1], param[2], param[3], param[4]);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.view_message, menu);
+        return true;
+    }
+
+}
